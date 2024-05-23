5e45c6fa9b4924555e507ea9f82b46bfcf4663e7
Dominik Hahn <develop.illuminati@gmail.com>
2024-05-23 16:59:30 +0200

    add route 53 service

---
+++ pkg/infrastructure/route53/route53.go
@@ -0,0 +1,30 @@
+package route53
+
+import (
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/apigatewayv2"
+	"github.com/pulumi/pulumi-aws/sdk/v6/go/aws/route53"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func Zone(ctx *pulumi.Context, name string, url string) (*route53.Zone, error) {
+	zone, err := route53.NewZone(ctx, name, &route53.ZoneArgs{
+		Name: pulumi.String(url),
+	})
+	return zone, err
+}
+
+func Record(ctx *pulumi.Context, name string, zone *route53.Zone, apigateway *apigatewayv2.Api) (*route53.Record, error) {
+	route, err := route53.NewRecord(ctx, name, &route53.RecordArgs{
+		Name: zone.Name,
+		Type: pulumi.String("A"),
+		Aliases: route53.RecordAliasArray{
+			&route53.RecordAliasArgs{
+				Name:                 apigateway.ApiEndpoint,
+				ZoneId:               pulumi.String("ZLY8HYME6SFDD"),
+				EvaluateTargetHealth: pulumi.Bool(false),
+			},
+		},
+		ZoneId: pulumi.String("ZLY8HYME6SFDD"), // the ID of your hosted zone in Route53
+	})
+	return route, err
+}
